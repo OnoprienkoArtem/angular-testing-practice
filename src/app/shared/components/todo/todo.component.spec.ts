9c7e61a9ce9a795e4c24e5329bad73b4dde326b7
OnoprienkoArtem <o.artem.e@gmail.com>
2024-10-08 23:35:16 +0300

    covered todo component by unit test

--- src/app/shared/components/todo/todo.component.spec.ts
+++ src/app/shared/components/todo/todo.component.spec.ts
@@ -4,6 +4,7 @@ import { TodosService } from '../../services/todos.service';
 import { TodoComponent } from '../todo/todo.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { By } from '@angular/platform-browser';
+import { first } from 'rxjs';
 
 
 describe('TodoComponent', () => {
@@ -45,5 +46,36 @@ describe('TodoComponent', () => {
     expect(label.nativeElement.textContent).toBe('foo');
   });
 
+  it('should toggle a todo', () => {
+    jest.spyOn(todosService, 'toggleTodo').mockImplementation(() => {});
+    const toggle = fixture.debugElement.query(By.css('[data-testid="toggle"]'));
+    toggle.nativeElement.click();
+    expect(todosService.toggleTodo).toHaveBeenCalledWith('1');
+  });
 
+  it('should remove a todo', () => {
+    jest.spyOn(todosService, 'removeTodo').mockImplementation(() => {});
+    const destroy = fixture.debugElement.query(By.css('[data-testid="destroy"]'));
+    destroy.nativeElement.click();
+    expect(todosService.removeTodo).toHaveBeenCalledWith('1');
+  });
+
+  it('should activate aditing', () => {
+    const label = fixture.debugElement.query(By.css('[data-testid="label"]'));
+    let clickedTodoId: string | null | undefined;
+    component.setEditingId.pipe(first()).subscribe(todoId => clickedTodoId = todoId);
+    label.triggerEventHandler('dblclick');
+    expect(clickedTodoId).toBe('1');
+  });
+
+  it('should change todo', () => {
+    jest.spyOn(todosService, 'changeTodo').mockImplementation(() => {});
+    component.isEditing = true;
+    fixture.detectChanges();
+    const edit = fixture.debugElement.query(By.css('[data-testid="edit"]'));
+    edit.nativeElement.value = 'foo';
+    edit.nativeElement.dispatchEvent(new KeyboardEvent('keyup', { key: 'Enter' }));
+
+    expect(todosService.changeTodo).toHaveBeenCalledWith('1', 'foo');
+  });
 });
